c2b8ca242ff0e381888563db6963a3dcd2e69f86
Karin Rimstidt <Karin Rimstidt>
2017-04-28 22:21:39 -0500

    committing homework

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name) VALUES ('Sorrowful Salsa Sting');
+INSERT INTO burgers (name, devoured) VALUES ('Hot Mustard Mess', true);
+INSERT INTO burgers (name, devoured) VALUES ('The American Hog', true);
+INSERT INTO burgers (name, devoured) VALUES ('The Basic American', true);
+INSERT INTO burgers (name) VALUES ('Blue Typhoon');
+INSERT INTO burgers (name) VALUES ('Hurricane Hannah');
+INSERT INTO burgers (name) VALUES ('Cali Veggie');
